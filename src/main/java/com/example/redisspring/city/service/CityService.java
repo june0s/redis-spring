001e91942ea0f409d6e66ffb25614fe569040b4d
june0 <june0@acornsoft.io>
2025-05-14 16:34:41 +0900

    RMapCacheReactive 로 변경 ttl 적용

--- src/main/java/com/example/redisspring/city/service/CityService.java
+++ src/main/java/com/example/redisspring/city/service/CityService.java
@@ -2,6 +2,7 @@ package com.example.redisspring.city.service;
 
 import com.example.redisspring.city.client.CityClient;
 import com.example.redisspring.city.dto.City;
+import org.redisson.api.RMapCacheReactive;
 import org.redisson.api.RMapReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.codec.TypedJsonJacksonCodec;
@@ -9,15 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class CityService {
 
     @Autowired
     private CityClient cityClient;
-    private RMapReactive<String, City> cityMap;
+    private RMapCacheReactive<String, City> cityMap;
 
     public CityService(RedissonReactiveClient client) {
-        this.cityMap = client.getMap("city", new TypedJsonJacksonCodec(String.class, City.class));
+        this.cityMap = client.getMapCache("city", new TypedJsonJacksonCodec(String.class, City.class));
     }
 
     /*
@@ -29,7 +32,7 @@ public class CityService {
     public Mono<City> getCity(final String zipCode) {
         return this.cityMap.get(zipCode)
                 .switchIfEmpty(this.cityClient.getCity(zipCode)
-                        .flatMap(c -> this.cityMap.fastPut(zipCode, c)
+                        .flatMap(c -> this.cityMap.fastPut(zipCode, c, 10, TimeUnit.SECONDS)
                                 .thenReturn(c))
                 );
     }
